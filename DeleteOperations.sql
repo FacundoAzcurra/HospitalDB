6d86825f96b3d98cf483f0a26b8d476aa6d14d0f
Facundo Azcurra Rech <fazcurra3@gmail.com>
2022-05-03 01:10:46 -0300

    Created operations

---
+++ DeleteOperations.sql
@@ -0,0 +1,20 @@
+DELETE FROM Appointments where idAppointments = 1; 
+
+DELETE FROM DeliveryRooms where idDeliveryRooms = 1;
+
+DELETE FROM DoctorOffice where idDoctorOffice = 1;
+
+DELETE FROM Managers where idManagers = 1;
+
+DELETE FROM MedicTrainees where idMedicTrainees = 1;
+
+DELETE FROM Patients where idPatients = 2;
+
+DELETE FROM Medics where idMedics = 1;
+
+DELETE FROM ChirophaneTrainees where idChirophanTrainees = 1;
+
+DELETE FROM Chirophane where idChirophane = 1;
+
+DELETE FROM Administratives where idAdministratives = 1;
+
---
+++ Manipulating.sql
@@ -0,0 +1,2 @@
+select* from Patients
+INNER JOIN Medics on Medics.idMedics = Patients.MedicID;
